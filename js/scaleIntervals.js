d3ac084c4e322b4fda25507333c873bcdb3ce229
Kevin Cozart <Cozartkevin@gmail.com>
2023-06-10 18:52:14 -0700

    Buttons for scales and keys fully functional
    MVP complete now to add fingering for left and right hand for each scale as well as musical representation of the scales too.

---
+++ js/scaleIntervals.js
@@ -0,0 +1,29 @@
+const scaleIntervals = {
+    "major": [0, 2, 4, 5, 7, 9, 11],
+    "natural-minor": [0, 2, 3, 5, 7, 8, 10],
+    "harmonic-minor": [0, 2, 3, 5, 7, 8, 11],
+    "melodic-minor": [0, 2, 3, 5, 7, 9, 11],
+    "dorian": [0, 2, 3, 5, 7, 9, 10],
+    "phrygian": [0, 1, 3, 5, 7, 8, 10],
+    "lydian": [0, 2, 4, 6, 7, 9, 11],
+    "mixolydian": [0, 2, 4, 5, 7, 9, 10],
+    "locrian": [0, 1, 3, 5, 6, 8, 10],
+    "pentatonic-major": [0, 2, 4, 7, 9],
+    "pentatonic-minor": [0, 3, 5, 7, 10],
+    "blues": [0, 3, 5, 6, 7, 10],
+    "whole-tone": [0, 2, 4, 6, 8, 10],
+    "chromatic": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+    "augmented": [0, 3, 4, 7, 8, 11],
+    "diminished": [0, 2, 3, 5, 6, 8, 9, 11],
+    "prometheus": [0, 2, 4, 6, 9, 10],
+    "tritone": [0, 1, 4, 6, 7, 10],
+    "enigmatic": [0, 1, 4, 6, 8, 10, 11],
+    "double-harmonic": [0, 1, 4, 5, 7, 8, 11],
+    "neapolitan-major": [0, 1, 3, 5, 7, 9, 11],
+    "neapolitan-minor": [0, 1, 3, 5, 7, 8, 11],
+    "major-blues": [0, 2, 3, 4, 7, 9],
+    "minor-blues": [0, 3, 5, 6, 7, 10],
+    "exotic": [0, 1, 4, 6, 8, 9, 11]
+  };
+
+  export default scaleIntervals
