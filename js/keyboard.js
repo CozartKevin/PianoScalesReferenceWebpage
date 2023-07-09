c65726cf4166869c39baff870b7598ba6f46f808
Raggdoll <Cozartkevin@gmail.com>
2023-07-09 15:49:09 -0700

    Organization changes, Fingerings introduction, and function renaming
    Renamed toggle functions to be more descriptive as they are setting the scale and key buttons to display selected.
    
    Added temporary fingerings for C# to A# as the program broke when toggling scales if there wasn't anything in the fingering object.
    
    Did some reworking of the Keyboard.css in an attempt to get the fingering object to display proper fingering numbers but wasn't successful yet.
    
    Added a TODO tracker as a VS Code extension and setup some known TODO tasks.
    
    Removed scalefingerings from keybaord.js as it has been broken out into a fingering.js file instead to save space.
    
    Changed noteMap inside getNoteByIndex to indexNoteMap to differentiate it from the other notemap object that uses strings to find index values unlike this indexNoteMap which uses integers to find strings.
    
    Modified some spacing throughout all documents.
    
    removed logging from setSelectedScaleButton (Formerly toggleScale) and reworked a few lines to reduce the amount of code.  Successfully compressed 3 lines into 2 but used one of them in two places.
    
    Reduced another call from 2 lines into 1 further down in the setSelectedScaleButton function.
    
    Moved SetSelectedScaleButton and SetSelectedKeyButton agove getSelectedScaleButton and getSelectedKeyButton so that getters and setters were near eachother for ease of reading.
    
    Added fingerings section to html document.

--- js/keyboard.js
+++ js/keyboard.js
@@ -54,103 +54,34 @@ function generatePianoKeyboard(startingNote, keysPerOctave, scaleType) {
   }
 
   highlightScale(scaleNotes);
-}
-
-
-
+};
 
 function highlightScale(scaleNotes) {
   const keyboard = document.querySelectorAll(".keyboard .white-key, .keyboard .black-key");
+  const Fingering = getFingerings();
+  const leftHand = Fingering.leftHand;
+  const rightHand = Fingering.rightHand;
+  const noteLabelStyleBefore = getComputedStyle(document.querySelector('.note-label'), '::before');
+  const noteLabelStyleAfter = getComputedStyle(document.querySelector('.note-label'), '::After');
+  const counter = 0;
+
 
   keyboard.forEach((key) => {
     const note = key.id;
 
     if (scaleNotes.includes(note)) {
       key.classList.add("scale-highlight");
+      noteLabelStyleBefore.setAttribute = ('Before-data-content', `"leftHand[${counter + 1}]"`);
+      noteLabelStyleAfter.setAttribute = ('After-data-content', `'rightHand[${counter + 1}]'`);
+
+   // console.log(noteLabelStyleBefore.content);
+   //  console.log(noteLabelStyleAfter.content);
+     
     } else {
       key.classList.remove("scale-highlight");
     }
   });
 }
-
-
-
-const scaleFingerings = {
-  "C": {
-    "major": {
-      "leftHand": [1, 2, 3, 1, 2, 3, 4, 1, 2, 3, 4, 5],
-      "rightHand": [1, 2, 3, 1, 2, 3, 4, 1, 2, 3, 4, 5]
-    },
-    "natural-minor": {
-      "leftHand": [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4],
-      "rightHand": [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]
-    },
-    "harmonic-minor": {
-      "leftHand": [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4],
-      "rightHand": [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]
-    },
-    "melodic-minor": {
-      "leftHand": [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4],
-      "rightHand": [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]
-    },
-    "dorian": {
-      "leftHand": [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4],
-      "rightHand": [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]
-    },
-    "phrygian": {
-      "leftHand": [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4],
-      "rightHand": [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]
-    }, "lydian": {
-      "leftHand": [1, 2, 3, 1, 2, 3, 4, 1, 2, 3, 4, 5],
-      "rightHand": [1, 2, 3, 1, 2, 3, 4, 1, 2, 3, 4, 5]
-    },
-    "mixolydian": {
-      "leftHand": [1, 2, 3, 1, 2, 3, 4, 1, 2, 3, 4, 5],
-      "rightHand": [1, 2, 3, 1, 2, 3, 4, 1, 2, 3, 4, 5]
-    },
-    "locrian": {
-      "leftHand": [1, 2, 3, 1, 2, 3, 4, 1, 2, 3, 4, 5],
-      "rightHand": [1, 2, 3, 1, 2, 3, 4, 1, 2, 3, 4, 5]
-    },
-    "pentatonic-major": {
-      "leftHand": [1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3],
-      "rightHand": [1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3]
-    },
-    "pentatonic-minor": {
-      "leftHand": [1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3],
-      "rightHand": [1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3]
-    },
-    "blues": {
-      "leftHand": [1, 2, 3, 4, 1, 2, 3, 1, 2, 3, 4, 1],
-      "rightHand": [1, 2, 3, 4, 1, 2, 3, 1, 2, 3, 4, 1]
-    },
-    "whole-tone": {
-      "leftHand": [1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3],
-      "rightHand": [1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3]
-    },
-    "chromatic": {
-      "leftHand": [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2],
-      "rightHand": [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2]
-    },
-    "augmented": {
-      "leftHand": [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4],
-      "rightHand": [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]
-    },
-  },
-  "C#": {
-    // Fingerings for "C#"...
-  },
-  "Db": {
-    // Fingerings for "Db"...
-  },
-  "D": {
-    // Fingerings for "D"...
-  },
-  // Add fingerings for other keys...
-};
-
-
-
 
 const scaleIntervals = {
   "major": [0, 2, 4, 5, 7, 9, 11],
@@ -219,7 +150,7 @@ function generateScaleNotes(startingNote, scaleType) {
 }
 
 function getNoteByIndex(noteIndex) {
-  const noteMap = {
+  const indexNoteMap = {
     0: "C",
     1: "C#",
     2: "D",
@@ -234,13 +165,12 @@ function getNoteByIndex(noteIndex) {
     11: "B"
   };
 
-  return noteMap[noteIndex];
+  return indexNoteMap[noteIndex];
 }
 
 function isBlackKey(note) {
   return note.includes("#") || note.includes("b");
 }
-
 
 function deletePianoKeyboard() {
   const pianoContainer = document.getElementById('piano-keyboard');
@@ -250,39 +180,34 @@ function deletePianoKeyboard() {
 
 
 // Function to toggle the scale type
-function toggleScale(scaleType) {
+function setSelectedScaleButton(scaleType) {
   // Remove highlight from all scale buttons
   const scaleButtons = document.querySelectorAll('.button-scale');
   scaleButtons.forEach(button => {
     button.classList.remove('active');
     button.disabled = false; // Enable all buttons
   });
-
+  const keyNote = getSelectedKeyButton();
 
   //remove Key Highlighting
   deletePianoKeyboard();
 
   // Generate the piano keyboard with the updated scale
-  const startingNote = getSelectedKey();
-  const keysPerOctave = getSelectedKeysPerOctave();
-  console.log(scaleType);
-  console.log(getSelectedKey());
-  console.log(getSelectedKeysPerOctave());
-  generatePianoKeyboard(startingNote, keysPerOctave, scaleType);
+  generatePianoKeyboard(keyNote, getSelectedKeysPerOctave(), scaleType);
 
   //Sending currently selected Key and the toggled scaletype to scale notes. 
-  const scaleNotes = generateScaleNotes(getSelectedKey(), scaleType);
-  highlightScale(scaleNotes);
+  highlightScale(generateScaleNotes(keyNote, scaleType));
 
   // Add highlight to the selected scale button
   const selectedButton = document.getElementById(`${scaleType}ScaleButton`);
   selectedButton.classList.add('active');
   selectedButton.disabled = true;
+  console.log("Out:" + scaleType)
 
 }
 
 // Function to toggle the starting key
-function toggleKey(startingNote) {
+function setSelectedKeyButton(startingNote) {
   // Remove highlight from all key buttons
   const keyButtons = document.querySelectorAll('.button-key');
   keyButtons.forEach(button => {
@@ -293,7 +218,7 @@ function toggleKey(startingNote) {
   deletePianoKeyboard();
 
   //Generate the piano keyboard with the updated starting key
-  generatePianoKeyboard(startingNote, getSelectedKeysPerOctave(), getSelectedScale());
+  generatePianoKeyboard(startingNote, getSelectedKeysPerOctave(), getSelectedScaleButton());
 
   // Add highlight to the selected key button
   const selectedButton = document.getElementById(`${startingNote}KeyButton`);
@@ -303,38 +228,42 @@ function toggleKey(startingNote) {
 
 }
 
-// Function to get the selected scale
-function getSelectedScale() {
-  const scaleButtons = document.querySelectorAll('.button-scale');
-  for (let i = 0; i < scaleButtons.length; i++) {
-    if (scaleButtons[i].classList.contains('active')) {
 
-      return scaleButtons[i].textContent;
-    }
-  }
-  return 'major'; // Default scale
-}
-
-// Function to get the selected starting key
-function getSelectedKey() {
-  const keyButtons = document.querySelectorAll('.button-key');
-  for (let i = 0; i < keyButtons.length; i++) {
-    if (keyButtons[i].classList.contains('active')) {
-      const buttonText = keyButtons[i].textContent;
-      const keyName = buttonText.includes('\\') ? buttonText.split('\\')[0].trim() : buttonText.trim();
-      return keyName;
-    }
-  }
-  return 'C'; // Default starting note
-}
-
+ // TODO: Implement additional octives in this funciton by adding a number of octives 
 // Function to get the selected keys per octave
 /// multiple octives math is 13 + 12*#of octives. if 1 then 13, if 2 then 25, if 3 then 37, 4 = 49, 5 = 61,6 = 73,7 = 85
 function getSelectedKeysPerOctave() {
   return 13; // Default value
 }
 
+// Function to get the currently selected scale button textContent
+function getSelectedScaleButton() {
+  const scaleButtons = document.querySelectorAll('.button-scale');
+  for (let i = 0; i < scaleButtons.length; i++) {
+   
+    if (scaleButtons[i].classList.contains('active')) {
+      console.log("scaleID: " + scaleButtons[i].id);
+      console.log("scaleTextContent: " + scaleButtons[i].textContent)
+      return scaleButtons[i].textContent;
+    }
+  }
+  return 'major'; // Default scale
+}
 
+// Function to gets the current selected Note Key button textContent
+// it splits out the sharp and flats and uses the Sharp name for the note. 
+function getSelectedKeyButton() {
+  const keyButtons = document.querySelectorAll('.button-key');
+  for (let i = 0; i < keyButtons.length; i++) {
+    if (keyButtons[i].classList.contains('active')) {
+      const buttonText = keyButtons[i].textContent;
+      const keyName = buttonText.includes('\\') ? buttonText.split('\\')[0].trim() : buttonText.trim();
+      console.log(keyName);
+      return keyName;
+    }
+  }
+  return 'C'; // Default starting note
+}
 function generateScaleButtons() {
   // Get the container element to append the buttons
   const buttonContainer = document.querySelector('.button-container-scale');
@@ -351,14 +280,12 @@ function generateScaleButtons() {
     button.id = `${scale}ScaleButton`;
     button.className = 'button-scale';
     button.textContent = scale;
-    button.onclick = () => toggleScale(scale);
+    button.onclick = () => setSelectedScaleButton(scale);
 
     // Append the button to the container
     buttonContainer.appendChild(button);
   }
 }
-
-
 
 function generateKeyButtons() {
   // Get the container element to append the buttons
@@ -388,11 +315,15 @@ function generateKeyButtons() {
       button.textContent = key;
     }
 
-    button.onclick = () => toggleKey(key);
+    button.onclick = () => setSelectedKeyButton(key);
 
     // Append the button to the container
     buttonContainer.appendChild(button);
   }
+}
+
+function getFingerings() {
+  return scaleFingerings[getSelectedKeyButton()][getSelectedScaleButton()];
 }
 
 
