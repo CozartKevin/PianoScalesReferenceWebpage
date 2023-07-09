9a4efa7b863076bc5d22a78dbab12268cb96ad13
Raggdoll <Cozartkevin@gmail.com>
2023-07-09 10:00:51 -0700

    Refactored File Structure
    Moved Files around so that the Project folder is a bit more Organized and the working files are easier to find.

---
+++ js/Backup_Of_Previous_Push_JS/fingering.BAK.js
@@ -0,0 +1,105 @@
+const scaleFingerings = {
+  "C": {
+    "major": {
+      "leftHand": [5, 4, 3, 2, 1, 3, 2, 1],
+      "rightHand": [1, 2, 3, 1, 2, 3, 4, 5]
+    },
+    "natural-minor": {
+      "leftHand": [5, 4, 3, 2, 1, 3, 2, 1],
+      "rightHand": [1, 2, 3, 1, 2, 3, 4, 5]
+    },
+    // Fill in the remaining scales with proper fingerings
+    "harmonic-minor": {
+      "leftHand": [5, 4, 3, 2, 1, 3, 2, 1],
+      "rightHand": [1, 2, 3, 1, 2, 3, 4, 5]
+    },
+    "melodic-minor": {
+      "leftHand": [5, 4, 3, 2, 1, 3, 2, 1],
+      "rightHand": [1, 2, 3, 1, 2, 3, 4, 5]
+    },
+    "dorian": {
+      "leftHand": [5, 4, 3, 2, 1, 3, 2, 1],
+      "rightHand": [1, 2, 3, 1, 2, 3, 4, 5]
+    },
+    "phrygian": {
+      "leftHand": [5, 4, 3, 2, 1, 3, 2, 1],
+      "rightHand": [1, 2, 3, 1, 2, 3, 4, 5]
+    },
+    "lydian": {
+      "leftHand": [1, 2, 3, 4, 1, 2, 3, 4],
+      "rightHand": [1, 2, 3, 4, 1, 2, 3, 4]
+    },
+    "mixolydian": {
+      "leftHand": [5, 4, 3, 2, 1, 3, 2, 1],
+      "rightHand": [1, 2, 3, 1, 2, 3, 4, 5]
+    },
+    "locrian": {
+      "leftHand": [5, 4, 3, 2, 1, 3, 2, 1],
+      "rightHand": [1, 2, 3, 1, 2, 3, 4, 5]
+    },
+    "pentatonic-major": {
+      "leftHand": [3, 2, 1, 3, 2, 1],
+      "rightHand": [1, 2, 3, 1, 2, 3]
+    },
+    "pentatonic-minor": {
+      "leftHand": [1, 3, 2, 1, 2, 1],
+      "rightHand": [1, 2, 3, 1, 2, 3]
+    },
+    "blues": { //REMOVE? We have blues minor & major later
+      "leftHand": [1, 2, 3, 4, 1, 2, 3, 4],
+      "rightHand": [1, 2, 3, 4, 1, 2, 3, 4]
+    },
+    "whole-tone": {
+      "leftHand": [3, 2, 1, 4, 3, 2, 1],
+      "rightHand": [1, 2, 3, 4, 1, 2, 3, 4]
+    },
+    "chromatic": {
+      "leftHand": [1, 3, 1, 3, 2, 1, 3, 1, 3, 1, 3, 2, 1],
+      "rightHand": [1, 3, 1, 3, 1, 2, 3, 1, 3, 1, 3, 1, 2]
+    },
+    "augmented": {
+      "leftHand": [5, 3, 2, 1, 3, 2, 2],
+      "rightHand": [1, 3, 1, 2, 3, 4, 5]
+    },
+    "diminished": {
+      "leftHand": [5, 4, 3, 1, 5, 4, 3, 2, 1],
+      "rightHand": [1, 2, 3, 1, 2, 3, 1, 2, 3]
+    },
+    "prometheus": {
+      "leftHand": [5, 4, 3, 2, 1, 2, 1],
+      "rightHand": [1, 2, 1, 2, 3, 4, 5]
+    },
+    "tritone": {  //not really a scale so remove?
+      "leftHand": [1, 2, 3, 4, 1, 2, 3, 4],
+      "rightHand": [1, 2, 3, 4, 1, 2, 3, 4]
+    },
+    "enigmatic": { //Fingering?
+      "leftHand": [1, 2, 3, 4, 1, 2, 3, 4],
+      "rightHand": [1, 2, 3, 4, 1, 2, 3, 4]
+    },
+    "double-harmonic": {
+      "leftHand": [1, 2, 3, 4, 1, 2, 3, 4],
+      "rightHand": [1, 2, 3, 4, 1, 2, 3, 4]
+    },
+    "neapolitan-major": {
+      "leftHand": [1, 2, 3, 4, 1, 2, 3, 4],
+      "rightHand": [1, 2, 3, 4, 1, 2, 3, 4]
+    },
+    "neapolitan-minor": {
+      "leftHand": [1, 2, 3, 4, 1, 2, 3, 4],
+      "rightHand": [1, 2, 3, 4, 1, 2, 3, 4]
+    },
+    "major-blues": {
+      "leftHand": [1, 2, 3, 4, 1, 2, 3, 4],
+      "rightHand": [1, 2, 3, 4, 1, 2, 3, 4]
+    },
+    "minor-blues": {
+      "leftHand": [1, 2, 3, 4, 1, 2, 3, 4],
+      "rightHand": [1, 2, 3, 4, 1, 2, 3, 4]
+    },
+    "exotic": {
+      "leftHand": [1, 2, 3, 4, 1, 2, 3, 4],
+      "rightHand": [1, 2, 3, 4, 1, 2, 3, 4]
+    }
+  }
+};
