ca261dc99dc4cfaaf8a217c7bf9b5b5e2e45cb2c
Kevin Cozart <Cozartkevin@gmail.com>
2024-03-01 10:49:04 -0800

    Comments, initialize and function modifications
    Updated comments to comply with JSDoc standards.
    
    Added Initialize function to reduce HTML calls to JS script.
    
    Pulled vexflow out of Generate Piano into it's own function call that also calls generate piano.
    
    Moved a few functions around so we would group top down and improve readability of program flow.

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,31 +1,38 @@
 //HTML for Flat: &#9837
 //HTML for Sharp: &#9839
 
-/*******************************
- * SCALE Note Generation
- * Used to generate the notes in the scale based on the starting note and scale type using the scale interval section
- *******************************/
-function generateScaleNotes(startingNote, scaleType) {
-  const startingNoteIndex = getNoteMapByChar(startingNote);
-  const scaleNotes = [];
-  const intervals = scaleIntervals[scaleType] || scaleIntervals["major"];
 
-  for (const interval of intervals) {
-    const noteIndex = (startingNoteIndex + interval) % 12;
-    console.log(noteIndex);
-    const note = getNoteMapByIndex(noteIndex);
-    scaleNotes.push(note);
-  }
-
-  console.log(scaleNotes + "Scale Notes");
-
-  return scaleNotes;
+/**
+ * Initialize Scale Reference.
+ * Function to set up the initial state of the Scale Reference application by generating scale buttons,
+ * key buttons, and initializing the piano keyboard.
+ */
+function initializeScaleReference() {
+  generateScaleButtons();
+  generateKeyButtons();
+  generateKeyboardAndVexFlow();
 }
 
-/*******************************
- * KEYBOARD Layout Function
- * JavaScript code for automagically generating the piano keyboard
- *******************************/
+/**
+ * Generate Piano Keyboard and VexFlow
+ * Function to generate the piano keyboard and VexFlow representation based on the selected key and scale.
+ **/
+function generateKeyboardAndVexFlow() {
+  const startingNote = getSelectedKeyButton();
+  const scaleType = getSelectedScaleButton();
+
+  // Generate piano keyboard
+  generatePianoKeyboard();
+
+  // Generate VexFlow representation
+  generateVexFlowScale();
+}
+
+/**
+ * Generate Piano Keyboard
+ * Function to generate the piano keyboard based on the selected key and scale.
+ *
+ */
 function generatePianoKeyboard() {
 
  const startingNote = getSelectedKeyButton();
@@ -65,14 +72,60 @@ console.log (startingNote + "Starting note in Generate Keybaord")
 
   console.log("Sent to highlightScale(scalenotes) from generatePianoKeyboard");
   highlightScale(generateScaleNotes(startingNote, scaleType));
-
-  generateVexFlowScale();
 }
 
 
-/*******************************
- * HIGHLIGHT Scale Function Place Fingerings
- *******************************/
+/**
+ * Generate VexFlow Code for Scales.
+ * Function to generate the VexFlow representation of scales.
+ */
+function generateVexFlowScale() {
+  // Get the container element for VexFlow
+  const vexFlowContainer = document.getElementById('vexflow-container');
+
+  // Clear previous VexFlow content
+  vexFlowContainer.innerHTML = '';
+
+  const scaleNotes = generateScaleNotes(getSelectedKeyButton(), getSelectedScaleButton());
+
+  // VexFlow setup
+  const renderer = new Vex.Flow.Renderer(vexFlowContainer, Vex.Flow.Renderer.Backends.SVG);
+  const context = renderer.getContext();
+  context.setFont('Arial', 10);
+
+  // Create a stave of width 400 at position 10, 40 with treble clef
+  const stave = new Vex.Flow.Stave(10, 40, 400);
+  stave.addClef('treble');
+
+  // Connect stave to the rendering context and draw
+  stave.setContext(context).draw();
+
+  // Create a voice and add dynamically generated notes
+  const voice = new Vex.Flow.Voice({ num_beats: scaleNotes.length, beat_value: 4 });
+
+  scaleNotes.forEach(note => {
+    const staveNote = new Vex.Flow.StaveNote({
+      keys: [`${note}/4`], // Assuming quarter notes for simplicity
+      duration: 'q'
+    });
+    voice.addTickable(staveNote);
+  });
+
+  // Format and render the voice
+  const formatter = new Vex.Flow.Formatter().joinVoices([voice]).format([voice], 350);
+  voice.draw(context, stave, formatter);
+
+  // Render
+  renderer.resize(500, 200);
+  renderer.getContext().scale(1.25, 1.25); // Adjust scale as needed
+  //renderer.draw();
+}
+
+
+/**
+ * Highlight Scale Function Place Fingerings.
+ * Function to highlight keys on the piano keyboard and place fingerings.
+ */
 function highlightScale(scaleNotes) {
   const keyboard = document.querySelectorAll(".keyboard .white-key, .keyboard .black-key");
 
@@ -107,10 +160,17 @@ function highlightScale(scaleNotes) {
 }
 
 
-/*******************************
- * Generate Scale Buttons
+
+/************************************************************************************************************************************************/
+
+/***************************
+*Button Generation Functions
+***************************/
+
+/**
+ * SCALE Button Generation.
  * Function sets up the initial Scale Buttons in the "Options" Menu under heading "Scales:"
- *******************************/
+ */
 function generateScaleButtons() {
   // Get the container element to append the buttons
   const buttonContainer = document.querySelector('.button-container-scale');
@@ -132,10 +192,10 @@ function generateScaleButtons() {
 }
 
 
-/*******************************
- * Generate Key Buttons
+/**
+ * KEY Button Generation.
  * Function sets up the initial Key Buttons in the "Options" Menu under heading "Keys:"
- *******************************/
+ */
 function generateKeyButtons() {
   // Mapping of notes to their corresponding positions
   const noteMap = {
@@ -181,54 +241,6 @@ const keys = Object.keys(noteMap);
   };
 
 
-/*******************************
- * Generate VexFlow Code for Scales
- *******************************/
-function generateVexFlowScale() {
-  // Get the container element for VexFlow
-  const vexFlowContainer = document.getElementById('vexflow-container');
-
-  // Clear previous VexFlow content
-  vexFlowContainer.innerHTML = '';
-
-  const scaleNotes = generateScaleNotes(getSelectedKeyButton(), getSelectedScaleButton());
-
-  // VexFlow setup
-  const renderer = new Vex.Flow.Renderer(vexFlowContainer, Vex.Flow.Renderer.Backends.SVG);
-  const context = renderer.getContext();
-  context.setFont('Arial', 10);
-
-  // Create a stave of width 400 at position 10, 40 with treble clef
-  const stave = new Vex.Flow.Stave(10, 40, 400);
-  stave.addClef('treble');
-
-  // Connect stave to the rendering context and draw
-  stave.setContext(context).draw();
-
-  // Create a voice and add dynamically generated notes
-  const voice = new Vex.Flow.Voice({ num_beats: scaleNotes.length, beat_value: 4 });
-
-  scaleNotes.forEach(note => {
-    const staveNote = new Vex.Flow.StaveNote({
-      keys: [`${note}/4`], // Assuming quarter notes for simplicity
-      duration: 'q'
-    });
-    voice.addTickable(staveNote);
-  });
-
-  // Format and render the voice
-  const formatter = new Vex.Flow.Formatter().joinVoices([voice]).format([voice], 350);
-  voice.draw(context, stave, formatter);
-
-  // Render
-  renderer.resize(500, 200);
-  renderer.getContext().scale(1.25, 1.25); // Adjust scale as needed
-  //renderer.draw();
-}
-
-
-
-
 
 
 /************************************************************************************************************************************************/
@@ -238,11 +250,43 @@ Helper Functions
 
 *******************************/
 
+
+/*******************************
+ * SCALE Note Generation.
+ * Used to generate the notes in the scale based on the starting note and scale type using the scale interval section.
+ *
+ * @param {string} startingNote - The starting note for the scale.
+ * @param {string} scaleType - The type of scale.
+ * @returns {string[]} An array of notes in the generated scale.
+ */
+function generateScaleNotes(startingNote, scaleType) {
+  const startingNoteIndex = getNoteMapByChar(startingNote);
+  const scaleNotes = [];
+  const intervals = scaleIntervals[scaleType] || scaleIntervals["major"];
+
+  for (const interval of intervals) {
+    const noteIndex = (startingNoteIndex + interval) % 12;
+    console.log(noteIndex);
+    const note = getNoteMapByIndex(noteIndex);
+    scaleNotes.push(note);
+  }
+
+  console.log(scaleNotes + "Scale Notes");
+
+  return scaleNotes;
+}
+
+
 /*******************************
  * SCALE Button set and get functions
  *******************************/
 
-// Function: Sets the selected ScaleType as Active
+
+/**
+ * Sets the selected ScaleType as Active.
+ *
+ * @param {string} scaleType - The type of scale to set as active.
+ */
 function setSelectedScaleButton(scaleType) {
   // Remove highlight from all scale buttons and enable them
   const scaleButtons = document.querySelectorAll('.button-scale');
@@ -254,8 +298,9 @@ function setSelectedScaleButton(scaleType) {
   // Get the currently selected key
   const keyNote = getSelectedKeyButton();
 
-  // Remove key highlighting
-  deletePianoKeyboard();
+   // Delete previous piano to generate the next one starting at the startingNote
+   //deletePianoKeyboard();  - Uncomment this line if you want to remove the "Animation" when changing scales via the buttons (Future toggle option?)
+
 
   // Add highlight to the selected scale button
   const selectedButton = document.getElementById(`${scaleType}ScaleButton`);
@@ -265,13 +310,17 @@ function setSelectedScaleButton(scaleType) {
     console.log("Out:" + scaleType);
 
     // Generate the piano keyboard with the updated scale
-    generatePianoKeyboard();
+    generateKeyboardAndVexFlow();
 
   }
 }
 
 
-// Function: Gets the current active ScaleType
+/**
+ * Gets the current active ScaleType.
+ *
+ * @returns {string} The active scale type.
+ */
 function getSelectedScaleButton() {
   const scaleButtons = document.querySelectorAll('.button-scale');
 
@@ -296,7 +345,11 @@ function getSelectedScaleButton() {
  * KEY Button set and get functions
  *******************************/
 
-// Function: Sets the selected KeyType as Active
+/**
+ * Sets the selected KeyType as Active.
+ *
+ * @param {string} startingNote - The starting note to set as active.
+ */
 function setSelectedKeyButton(startingNote) {
    // Remove highlight from all key buttons and enable them
    const keyButtons = document.querySelectorAll('.button-key');
@@ -306,7 +359,7 @@ function setSelectedKeyButton(startingNote) {
    });
   
    // Delete previous piano to generate the next one starting at the startingNote
-   deletePianoKeyboard();
+   //deletePianoKeyboard(); - Uncomment this line if you want to remove the "Animation" when changing Keys via the buttons (Future toggle option?)
 
   // Add highlight to the selected key button
   const selectedButton = document.getElementById(`${startingNote}KeyButton`);
@@ -317,12 +370,16 @@ function setSelectedKeyButton(startingNote) {
 
   
  // Generate the piano keyboard with the updated starting key
- generatePianoKeyboard();
+ generateKeyboardAndVexFlow();
 }
 
 
-// Function Gets the current Active KeyType
-// it splits out the sharp and flats and uses the Sharp name for the note. 
+/**
+ * Gets the current Active KeyType.
+ * It splits out the sharp and flats and uses the Sharp name for the note.
+ *
+ * @returns {string} The active key type.
+ */
 function getSelectedKeyButton() {
   const keyButtons = document.querySelectorAll('.button-key');
   for (const keyButton of keyButtons) {
@@ -353,11 +410,13 @@ function extractKeyFromButtonText(buttonID) {
 }
 
 
-/****************************************************************************************************
- * ConvertToVexFlowFormat
- *  converts output from generateScaleNotes into a format that VexFlow can recognize in a voice. 
- ***************************************************************************************************/
-
+/**
+ * Convert To VexFlow Format
+ * Converts output from generateScaleNotes into a format that VexFlow can recognize in a voice.
+ *
+ * @param {string[]} scaleNotes - An array of notes in the scale.
+ * @returns {string[]} An array of notes in VexFlow format.
+ */
 function convertToVexFlowFormat(scaleNotes) {
   const vexFlowNotes = [];
 
@@ -376,9 +435,17 @@ function convertToVexFlowFormat(scaleNotes) {
 Octive Button set and get functions
 
 *******************************/
+
+
 // TODO: Implement additional octives in this funciton by adding a number of octives 
-// Function to get the selected keys per octave
-/// multiple octives math is 13 + 12*#of octives. if 1 then 13, if 2 then 25, if 3 then 37, 4 = 49, 5 = 61,6 = 73,7 = 85
+
+/**
+ * Get Selected Keys per Octave
+ * Gets the selected keys per octave.
+ * Multiple octaves math is 13 + 12 * #of octaves. If 1, then 13; if 2, then 25; if 3, then 37; 4 = 49; 5 = 61; 6 = 73; 7 = 85.
+ *
+ * @returns {number} The number of keys per octave.
+ */
 function getSelectedKeysPerOctave() {
   return 13; // Default value
 }
@@ -389,7 +456,13 @@ NOTE Mapping Get functions
 //Get by Character and by Index
 *******************************/
 
-//Function returns the index of the character sent into it based on the piano key names
+/**
+ * Get Note Map by Char
+ * Returns the index of the character sent into it based on the piano key names.
+ *
+ * @param {string} noteChar - The character representing the note.
+ * @returns {number} The index of the note.
+ */
 function getNoteMapByChar(noteChar) {
   const charNoteMap = {
     "C": 0,
@@ -418,8 +491,14 @@ function getNoteMapByChar(noteChar) {
   return noteIndex;
 }
   
-//Doesn't reference flat keys as the indexing is the same as sharps
-//Code in functions add in flat when needed but reference the sharp only
+/**
+ * Get Note Map by index
+ * Doesn't reference flat keys as the indexing is the same as sharps.
+ * Code in functions add in flat when needed but reference the sharp only.
+ *
+ * @param {number} noteIndex - The index of the note.
+ * @returns {string} The note name.
+ */
 function getNoteMapByIndex(noteIndex) {
   const indexNoteMap = {
     0: "C",
@@ -441,12 +520,22 @@ function getNoteMapByIndex(noteIndex) {
   return indexNoteMap[noteIndex];
 }
 
-//Returns true or false based on if the inbound string has a sharp or flat symbol in it.
+/**
+ * Is Black Key
+ * Returns true or false based on if the inbound string has a sharp or flat symbol in it.
+ *
+ * @param {string} note - The note name.
+ * @returns {boolean} True if the note is a black key, false otherwise.
+ */
 function isBlackKey(note) {
   return note.includes("#") || note.includes("b");
 }
 
-//Deletes the keyboard completely by using the getELementsByID and replacing the internal HTML with an empty string.
+/**
+ * Delete Piano keyboard
+ * Deletes the keyboard completely by using the getElementsById and replacing the internal HTML with an empty string.
+ */
+//NOTE: If you uncomment the deletePianoKeyboard in the setSelectedScaleButton and setSelectedKeyButton you remove the smooth animation the highlighting does. Make feature for toggling on and off the animation. 
 function deletePianoKeyboard() {
   const pianoContainer = document.getElementById('piano-keyboard');
   pianoContainer.innerHTML = '';
@@ -457,6 +546,13 @@ FINGERING function
 //TODO: Need to impliment getting the fingerings from the fignerings.js file and load them in the
 // ::BEFORE (Left hand fingerings) & ::AFTER(Right hand fingerings) areas of each highlighted key.  
 *******************************/
+
+/**
+ * Get Left Fingerings
+ * Gets the left hand fingerings for the selected key and scale.
+ *
+ * @returns {number[]} An array of left hand fingerings.
+ */
 function getLeftFingerings() {
   return scaleFingerings[getSelectedKeyButton()][getSelectedScaleButton()].lefthand;
 }
@@ -466,8 +562,12 @@ OBJECTS
 
 *******************************/
 
-//Object holds all numeric spacing for each scale based on the index there being 12 unique keys.
-//It should be noted that we duplicate the name of the initial key at the end of each octive so the displayed board is 13 keys total. 
+/**
+ * Object holds all numeric spacing for each scale based on the index, there being 12 unique keys.
+ * It should be noted that we duplicate the name of the initial key at the end of each octave so the displayed board is 13 keys total.
+ *
+ * @type {Object.<string, number[]>}
+ */
 const scaleIntervals = {
   "major": [0, 2, 4, 5, 7, 9, 11],
   "natural-minor": [0, 2, 3, 5, 7, 8, 10],
